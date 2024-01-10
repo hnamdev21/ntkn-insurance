7b31bb6dd838bd1c143e194271f4a5ccc4676659
Jackie <1121@generative.xyz>
2024-01-11 04:27:45 +0700

    feat(page/home): basic bento grid

--- src/modules/User/HomePageSections/Bento/index.tsx
+++ src/modules/User/HomePageSections/Bento/index.tsx
@@ -6,8 +6,18 @@ import Section from "@/components/Section";
 const BentoSection = () => {
   return (
     <Section>
-      <Container>
-        <h1>BentoSection</h1>
+      <Container className="flex flex-col md:gap-[1.8rem]">
+        <div className="md:h-[44rem] w-full border border-1 border-slate-950 rounded-3xl"></div>
+
+        <div className="flex gap-[1.8rem] md:h-[44rem] w-full">
+          <div className="border border-1 border-slate-950 md:rounded-3xl md:w-2/3"></div>
+          <div className="border border-1 border-slate-950 md:rounded-3xl md:w-1/3"></div>
+        </div>
+
+        <div className="flex gap-[1.8rem] md:h-[44rem] w-full">
+          <div className="border border-1 border-slate-950 md:rounded-3xl md:w-1/3"></div>
+          <div className="border border-1 border-slate-950 md:rounded-3xl md:w-2/3"></div>
+        </div>
       </Container>
     </Section>
   );
